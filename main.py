3c591665a9621b11f43b364765d7cbfc5c7f3462
stivenmoscoso <103283733+stivenmoscoso@users.noreply.github.com>
2025-11-24 13:34:06 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+from servicios import (register_product, consult_book,update_product,delete_product, register_sales, top_3_sales, sales_author,inventory)
+
+
+def main():
+
+    while True:
+        print("----SISTEMA------")
+        print("____________________")
+        print("1. Add New Book")
+        print("2. Search Book")
+        print("3. Update Book")
+        print("4. Delete Book")
+        print("5. Register Sale")
+        print("6. Report: Top 3 of Sale")
+        print("7. Report: Sale by Author")
+        print("0. Salir")
+        print("_____________________")
+
+        opcion = input("Select one of the following options: ")
+        
+        if opcion == "1":
+            title = input("Book Title: ")
+            author = input("Author Name: ")
+            category = input("Book Category: ")
+            price = input("Book Price: ")
+            stock = input("Stock:  ")
+            register_product(title,author,category, price, stock)
+            print
+
+        elif opcion == "2":
+            title = input("Write the title of the book: ")
+            libro = consult_book(inventory, title)
+            print(libro if libro else "Not Found")
+
+        elif opcion == "3":
+            title = input("book title to update: ")
+            new_author = input("Enter New Author: ")
+            new_category = input("Enter New Category: ")
+            new_price = input("Enter New Price: ")
+            new_stock = input("Enter New Stock: ")
+            update_product(inventory,title,new_author,new_category,new_price, new_stock)
+        elif opcion == "4":
+            title = input("Book title for delete: ")
+            delete_product(inventory, title)
+
+        elif opcion == "5":
+            register_sales(inventory)
+
+        elif opcion == "6":
+            top_3_sales()
+
+        elif opcion == "7":
+            sales_author()
+
+        elif opcion == "0":
+             print("Exit, Thanks")
+             break
+             
+        else: print("Invalid option, please correct")
+
+main()
---
+++ servicios.py
@@ -0,0 +1,154 @@
+# Registration Module 
+inventory = [{'title': 'el principito', 'author': 'Stiven', 'category': 'fabula', 'price': 2000, 'stock': 50,},
+             {'title': 'don quijote', 'author': 'Stiven', 'category':'clasico', 'price': 4000, 'stock': 30,},
+             {'title': '1983', 'author': 'Carlos', 'category':'novela', 'price' : 1000, 'stock': 20,},
+             {'title': 'python', 'author': 'Juan', 'category': 'programacion', 'price': 2500, 'stock': 100,},
+             {'title': 'poemas', 'author': 'Stiven', 'category': 'fabula', 'poemas clasicos''price' :800, 'stock': 15,},
+             ]
+
+            
+
+
+def register_product(title, author, category,price, stock):
+    book = {
+        'title': title,
+        'author': author,
+        'category': category,
+        'price': price,
+        'stock': stock
+    }
+    inventory.append(book)
+    print (f"Book: '{title}' registered successfully.")
+
+# funtion to consult books
+def consult_book(inventory, name_search):
+    for book in inventory:
+     if book["title"] == name_search.lower():
+        return book
+    return None
+# function to update book information
+    
+def update_product(inventory, title, new_author, new_category, new_price, new_stock):
+
+    book = consult_book(inventory,title)
+    if book is None:
+        print("Book not found.")
+        return
+    else:
+        if new_author != "":
+            book['author'] = new_author
+        if new_category != "":
+            book['category'] = new_category
+        if new_stock != "":
+           try:
+             book['stock'] = int(new_stock)
+           except ValueError:
+               print("Stock invalid, Not Update")
+        if new_price != "":
+           try:
+             book['price'] = float(new_price)
+           except ValueError:
+               print("Price invalid, Not Update")
+        print(f"{title} Update successfully")
+        
+
+# function to delete a book from inventory
+def delete_product(inventory, title):
+    book = consult_book(inventory, title)
+    if book == None:
+        print("Book not found.")
+        return
+    else:
+        inventory.remove(book)
+        print(f"Book '{title}' deleted successfully.")
+
+
+# Sales Module
+sales = []
+def register_sales(inventory):
+    print("-----Register Sale-----")
+    customer_name = input("Enter Customer Name: ")
+    type_client = input("Enter Type of Client (Regular o VIP): ")
+    book_title_sale = input("Enter The Book Title sold: ").lower()
+
+    book = consult_book(inventory, book_title_sale)
+
+    if book is None:
+        print("Book not found in inventory.")
+        return
+    quantity = int(input("Enter Quantity sold: "))   
+
+    if quantity > book['stock']:
+        print("Insufficient stock for the sale.")
+        return 
+    
+    #time of sale
+    from datetime import datetime
+    time = datetime.now().strftime("%Y-%m-%d %H:%M")
+
+    #descount 
+    descuento = float(input("Ingrese el Descuento aplicado:"))
+    #final price
+    final_price = (book['price'] * quantity) - descuento
+
+#register sale
+    sale = {
+        'customer_name': customer_name,
+        'type_client': type_client,
+        'book_title_sale': book_title_sale,
+        'quantity': quantity,
+        'time': time,
+        'descuento': descuento,
+        'final_price': final_price
+    }
+    sales.append(sale)
+
+
+    #update stock
+    book['stock'] -= quantity
+
+    print(f"Sale registered successfully for '{book_title_sale}'.") 
+    print(f"Final Price after discount: ${final_price}")
+
+
+from collections import defaultdict
+
+#top 3 of sales
+def top_3_sales():
+    sales_count = defaultdict(int)
+
+    for sale in sales:
+        sales_count[sale['book_title_sale']] += sale['quantity']
+    
+    top_sales = sorted(sales_count.items(), key=lambda x: x[1], reverse=True)[:3]
+    print("---The 3 best-selling books---")
+    for sale, quantity in top_sales: 
+        print(f"Book:{sale}, Quantity: {quantity} unidades vendidas")
+
+
+#sales report for Author
+def sales_author():
+    if  not sales:
+        print("No hay ventas registradas")
+        return
+    
+    sold_authors = defaultdict(int) 
+
+    for sale in sales:
+     book = consult_book(inventory, sale['book_title_sale'])
+     if book:
+         sold_authors[book['author']] += sale['quantity']
+    print("---Ventas por Autor---")
+
+    for author, quantity in sold_authors.items():
+        print(f"Autor {author}, Cantidad Vendida {quantity} units sold")
+    
+  
+
+
+
+
+
+ 
+
+ 
